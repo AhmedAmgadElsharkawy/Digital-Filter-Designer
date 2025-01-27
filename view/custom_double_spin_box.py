eacc554003455b6f7e095cbb785cf27a4fbbe107
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2025-01-27 18:41:00 +0200

    add the filters library

---
+++ view/custom_double_spin_box.py
@@ -0,0 +1,22 @@
+from PyQt5.QtWidgets import QWidget,QHBoxLayout,QLabel,QDoubleSpinBox
+
+
+class CustomDoubleSpinBox(QWidget):
+    def __init__(self,label = "label",range_start = 0.00010,range_end = 0.49999,initial_value = 0.1,decimals = 5,step_value = 0.1):
+        super().__init__()
+        self.central_layout = QHBoxLayout(self)
+        self.main_widget = QWidget()
+        self.main_widget_layout = QHBoxLayout(self.main_widget)
+        self.central_layout.addWidget(self.main_widget)
+        
+        self.label = QLabel(label)
+        self.main_widget_layout.addWidget(self.label)
+        self.double_spin_box = QDoubleSpinBox()
+        self.double_spin_box.setRange(range_start, range_end)
+        self.double_spin_box.setValue(initial_value)
+        self.double_spin_box.setDecimals(decimals)
+        self.double_spin_box.setSingleStep(step_value)
+        self.main_widget_layout.addWidget(self.double_spin_box)
+
+        
+
--- view/main_window.py
+++ view/main_window.py
@@ -1,7 +1,8 @@
-from PyQt5.QtWidgets import QMainWindow,QWidget,QHBoxLayout,QVBoxLayout,QLabel,QPushButton
+from PyQt5.QtWidgets import QMainWindow,QWidget,QHBoxLayout,QVBoxLayout,QLabel,QPushButton,QComboBox,QSpinBox,QDoubleSpinBox
 
 from view.custom_z_plane import CustomZPlane
 from view.response_plot import ResponsePlot
+from view.custom_double_spin_box import CustomDoubleSpinBox
 
 
 class MainWindow(QMainWindow):
@@ -76,7 +77,61 @@ class MainWindow(QMainWindow):
         self.filter_response_plots_widget_layout.addWidget(self.filter_magnitude_response)
         self.filter_response_plots_widget_layout.addWidget(self.filter_phase_response)
         
+        self.filters_widget = QWidget()
+        self.filters_widget_layout = QVBoxLayout(self.filters_widget)
+        self.filter_row_layout.addWidget(self.filters_widget)
+        
+        self.filters_widget_header = QWidget()
+        self.filters_widget_header_layout = QHBoxLayout(self.filters_widget_header)
+        self.filters_widget_layout.addWidget(self.filters_widget_header)
+        filter_items = ['Butterworth Filter', 'Chebyshev Filter', 'inv Chebyshev Filter', 'Bessel Filter',"Elliptic Filter"]
+        self.filters_combobox = QComboBox()
+        self.filters_combobox.addItems(filter_items)
+        self.apply_filter_button = QPushButton("Apply Filter")
+        self.filters_widget_header_layout.addWidget(self.filters_combobox)
+        self.filters_widget_header_layout.addWidget(self.apply_filter_button)
 
+        self.filter_controls_widget = QWidget()
+        self.filter_controls_widget_layout = QVBoxLayout(self.filter_controls_widget)
+        self.filters_widget_layout.addWidget(self.filter_controls_widget)
+
+        self.filter_type_container = QWidget()
+        self.filter_type_container_layout = QHBoxLayout(self.filter_type_container)
+        self.filter_controls_widget_layout.addWidget(self.filter_type_container)
+        self.filter_type_label = QLabel("type")
+        self.filter_types = ['Low-pass', 'High-pass', 'Band-pass', 'Band-stop']
+        self.filter_type_combobox = QComboBox()
+        self.filter_type_combobox.addItems(self.filter_types)
+        self.filter_type_container_layout.addWidget(self.filter_type_label)
+        self.filter_type_container_layout.addWidget(self.filter_type_combobox)
+
+        self.filter_order_container = QWidget()
+        self.filter_order_container_layout = QHBoxLayout(self.filter_order_container)
+        self.filter_controls_widget_layout.addWidget(self.filter_order_container)
+        self.filter_order_label = QLabel("Order")
+        self.filter_order_container_layout.addWidget(self.filter_order_label)
+        self.filter_order_spin_box = QSpinBox()
+        self.filter_order_spin_box.setRange(1, 4)
+        self.filter_order_spin_box.setValue(1)
+        self.filter_order_container_layout.addWidget(self.filter_order_spin_box)
+
+
+        self.filter_start_frequency_container = CustomDoubleSpinBox(label="Start Frequency")
+        self.filter_controls_widget_layout.addWidget(self.filter_start_frequency_container)
+
+        self.filter_end_frequency_container = CustomDoubleSpinBox(label="End Frequency")
+        self.filter_controls_widget_layout.addWidget(self.filter_end_frequency_container)
+
+        self.transition_band_container = CustomDoubleSpinBox(label="Transition Band")
+        self.filter_controls_widget_layout.addWidget(self.filter_end_frequency_container)
+
+        self.passband_ripple_container = CustomDoubleSpinBox(label="Passband Ripple")
+        self.filter_controls_widget_layout.addWidget(self.passband_ripple_container)
+
+        self.stopband_ripple_contnainer = CustomDoubleSpinBox(label="Stopband Ripple")
+        self.filter_controls_widget_layout.addWidget(self.filter_end_frequency_container)
+        
+        
 
 
         self.setStyleSheet("""
