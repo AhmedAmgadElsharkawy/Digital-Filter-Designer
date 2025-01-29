812354af0ed28fd596faf09d6dc32a356cbec055
AhmedAmgadEid <ahmedamgad16746@gmail.com>
2025-01-29 20:22:42 +0200

    fix conj poles and zeroes add and remove filter functions

--- model/filter_model.py
+++ model/filter_model.py
@@ -7,7 +7,6 @@ class FilterModel:
         
     def add_pole(self,pole):
         self.poles.append(pole)
-        print("self.poles",self.poles)
 
     def remove_pole(self,pole):
         if pole in self.poles:
@@ -15,25 +14,35 @@ class FilterModel:
 
     def add_zero(self,zero):
         self.zeroes.append(zero)
-        print("self.zeroes",self.zeroes)
 
     def remove_zero(self,zero):
         if zero in self.zeroes:
             self.zeroes.remove(zero)
 
-    def add_conj_poles(self,conj_poles):
-        self.conj_poles.append(conj_poles)
-        print("self.conj_poles",self.conj_poles)
+    def add_conj_poles(self,pole):
+        self.conj_poles.append(pole)
 
-    def remove_conj_poles(self,conj_poles):
-        pass
+    def remove_conj_poles(self,pole):
+        conjugate_pole = complex(pole.real, -pole.imag)
+        if pole in self.conj_poles:
+            self.conj_poles.remove(pole)
+            return
+        if conjugate_pole in self.conj_poles:
+            self.conj_poles.remove(conjugate_pole)
+            return
     
-    def add_conj_zeroes(self,conj_zeroes):
-        self.conj_zeroes.append(conj_zeroes)
-        print("self.conj_zeroes",self.conj_zeroes)
+    def add_conj_zeroes(self,zero):
+        self.conj_zeroes.append(zero)
 
-    def remove_conj_zeroes(self,conj_zeroes):
-        pass
+    def remove_conj_zeroes(self,zero):
+        conjugate_zero = complex(zero.real, -zero.imag)
+        if zero in self.conj_zeroes:
+            self.conj_zeroes.remove(zero)
+            return
+        if conjugate_zero in self.conj_zeroes:
+            self.conj_zeroes.remove(conjugate_zero)
+            return 
+        
 
     def clear_all_polses(self):
         self.poles = []
